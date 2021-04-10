68fba1defb7185d92feafa888d4cb5600eb7062c
Mohnish Agrawal <mohnisha2011@icloud.com>
2021-04-10 14:29:27 +0400

    Added sections

--- runFront.py
+++ runFront.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -17,6 +18,17 @@ def right_sidebar():
 @app.route('/no-sidebar')
 def no_sidebar():
 	return render_template("no-sidebar.html")
+
+# @app.route('/upload')
+# def upload_file():
+#    return render_template('upload.html')
+	
+# @app.route('/uploader', methods = ['GET', 'POST'])
+# def uploadfile():
+#    if request.method == 'POST':
+#       f = request.files['file']
+#       f.save(secure_filename(f.filename))
+#       return 'file uploaded successfully'
 
 if __name__ == '__main__':
 	app.run(debug = True)
